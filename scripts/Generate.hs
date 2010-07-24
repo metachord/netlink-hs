aed4b0e29bad198b3de35b8cbec922ca5ec65f19
David Anderson <dave@natulte.net>
2010-07-25 01:54:34 +0200

    Add a script that auto-generates nices enums for Netlink constants.

---
+++ scripts/Generate.hs
@@ -0,0 +1,83 @@
+module Main where
+
+import Control.Applicative ((<$>))
+import Control.Arrow ((&&&))
+import Data.Char (isNumber, toLower, toUpper)
+import Data.List (intersperse, isPrefixOf, isInfixOf, notElem, elem)
+import Data.Set (fromList, toList)
+import System.Environment (getArgs)
+import System.Process (readProcess)
+
+main = do
+    [out] <- getArgs
+    inc <- includes
+    defs <- generate
+    writeFile out (inc ++ defs)
+
+includes :: IO String
+includes = includeBlock [ "linux/if.h"
+                        , "linux/if_tun.h"
+                        , "linux/if_arp.h"
+                        , "linux/netlink.h"
+                        , "linux/rtnetlink.h"
+                        , "sys/socket.h"
+                        ]
+
+generate :: IO String
+generate = ('\n' :) . concat <$> sequence
+       [ defsToEnum "LinkFlags" ["linux/if.h",
+                                 "linux/if_tun.h"] ("IFF_" `isPrefixOf`)
+       , defsToEnum "LinkType" ["linux/if_arp.h"] ("ARPHRD_" `isPrefixOf`)
+       , defsToEnum "MessageFlags" ["linux/netlink.h"] ("NLM_F_" `isPrefixOf`)
+       , defsToEnum "MessageType" ["linux/netlink.h",
+                                   "linux/rtnetlink.h"]
+         (\d -> d `notElem` ["NLMSG_ALIGNTO", "NLMSG_MIN_TYPE", "RTM_MAX"] &&
+                not ("RTM_F_" `isPrefixOf` d) &&
+                ("NLMSG_" `isPrefixOf` d || "RTM_" `isPrefixOf` d))
+       , defsToEnum "AddressFamily" ["sys/socket.h"] ("AF_" `isPrefixOf`)
+       ]
+
+defsToEnum :: String -> [String] -> (String -> Bool) -> IO String
+defsToEnum name patterns cond = do
+    makeEnum name <$> filteredDefs patterns cond
+
+makeEnum :: String -> [String] -> String
+makeEnum name defs = unlines $ header : body ++ ["};"]
+  where
+    header = "enum " ++ name ++ " {"
+    body = map fmt $ map (toCamelCase &&& id) defs
+    fmt (a,b) = "  " ++ a ++ " = " ++ b ++ ","
+
+filteredDefs :: [String] -> (String -> Bool) -> IO [String]
+filteredDefs patterns filt = filter filt <$> get
+  where get = getDefinitions =<< includeBlock patterns
+
+getDefinitions :: String -> IO [String]
+getDefinitions headers = uniq . clean <$> preprocess
+  where preprocess = readProcess "gcc" ["-E", "-dM", "-"] headers
+        uniq = toList . fromList
+        clean = map (!! 1) . filter interesting . map words . lines
+        interesting def = (isDefine def && hasValue def &&
+                           hasNumericValue def && not (isMacro def) &&
+                           not (isInternal def))
+        isDefine = (== "#define") . head
+        hasValue = (>= 3) . length
+        hasNumericValue = (/= '(') . head . (!! 2)
+        isMacro def = '(' `elem` (def !! 1)
+        isInternal def = head (def !! 1) == '_'
+
+includeBlock :: [String] -> IO String
+includeBlock patterns = unlines . mkInclude . clean . lines <$> get
+  where
+    get = concat <$> sequence (map getOne patterns)
+    getOne p = readProcess "find" [root, "-wholename", root ++ p] ""
+    clean = map $ drop (length root)
+    mkInclude = map (\x -> "#include <" ++ x ++ ">")
+    root = "/usr/include/"
+
+toCamelCase :: String -> String
+toCamelCase s = run s True
+  where run []       _     = []
+        run ('_':xs) _     = run xs True
+        run (x:xs)   True  = (toUpper x) : run xs False
+        run (x:xs)   False = (toLower x) : run xs False
