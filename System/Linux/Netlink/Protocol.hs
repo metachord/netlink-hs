61d2b4238a708845595aa367140cb0d5f44a223c
David Anderson <dave@natulte.net>
2010-07-25 06:02:33 +0200

    Start reimplementing System.Linux.Netlink.Protocol with a nicer structure.

--- System/Linux/Netlink/Protocol.hs
+++ System/Linux/Netlink/Protocol.hs
@@ -4,77 +4,124 @@ import Prelude hiding (length)
 
 import Control.Applicative ((<$>))
 import Data.ByteString (ByteString, length)
-import Data.Serialize (Serialize(..))
 import Data.Serialize.Get
 import Data.Serialize.Put
-import Data.Word (Word32)
-import Foreign.C.Error (Errno(..), errnoToIOError)
-import Foreign.C.Types (CInt)
+import Data.Word (Word8, Word16, Word32)
+import Foreign.C.Error (Errno(..))
 
 import System.Linux.Netlink.C
 
-data Message = GetInterface LinkType Word32 Word32
-             | NewInterface LinkType Word32 Word32
-             | Success
-             | Error CInt String
-             | Other MessageType
-             deriving (Eq, Show)
+data Header = Header
+    {
+      messageType   :: MessageType
+    , messageFlags  :: MessageFlags
+    , messageSeqNum :: Word32
+    , messagePID    :: Word32
+    } deriving (Eq, Show)
 
-instance Serialize Message where
-    put (GetInterface deviceType interfaceIndex linkFlags) =
-        putPacket RtmGetlink [NlmFRequest] $ runPut $ do
-            putWord8 (_fromEnum AfUnspec)
-            putWord8 0
-            putWord16host (_fromEnum deviceType)
-            putWord32host interfaceIndex
-            putWord32host linkFlags
-            putWord32host 0xFFFFFFFF
-    
-    put _ = fail "Unsupported userspace->kernel message type"
+data Message = ErrorMsg
+    {
+      errorCode   :: Errno
+    , errorHeader :: Header
+    }
+             | LinkMsg
+    {
+      interfaceType  :: LinkType
+    , interfaceIndex :: Word32
+    , interfaceFlags :: Word32
+    } deriving (Eq)
 
-    get = do
-        skip 4
-        msgTy <- _toEnum <$> getWord16host
-        skip 10
-        case msgTy of
-            NlmsgError -> getError
-            RtmGetlink -> getInterface
-            RtmNewlink -> newInterface
-            _          -> return $ Other msgTy
-      where
-        getError = do
-            code <- negate . fromIntegral <$> getWord32host
-            let err = errnoToIOError "recvmsg" (Errno code) Nothing Nothing
-                errStr = show err
-            if code == 0
-                then return Success
-                else return $ Error code errStr
+data Attributes = Attributes deriving (Eq, Show)
 
-        getInterface = do
-            skip 2
-            deviceType <- _toEnum <$> getWord16host
-            interfaceIndex <- getWord32host
-            flags <- getWord32host
-            return $ GetInterface deviceType interfaceIndex flags
-            
-        newInterface = do
-            skip 2
-            deviceType <- _toEnum <$> getWord16host
-            interfaceIndex <- getWord32host
-            flags <- getWord32host
-            return $ NewInterface deviceType interfaceIndex flags
+type Packet = (Header, Message, Attributes)
 
-putPacket :: MessageType -> [MessageFlags] -> ByteString -> Put
-putPacket msgTy flags payload = do
-    putWord32host . fromIntegral $ (length payload) + 16
-    putWord16host (_fromEnum msgTy)
-    putWord16host (setFlags flags)
-    putWord32host 1
-    putWord32host 0
-    putByteString payload
+--
+-- Packet decoding
+--
+getPacket :: Get Packet
+getPacket = do
+    header <- getHeader
+    msg    <- getMessage (messageType header)
+    attrs  <- getAttributes
+    return (header, msg, attrs)
 
-_fromEnum :: (Enum a, Num b) => a -> b
-_fromEnum = fromIntegral . fromEnum
+getHeader :: Get Header
+getHeader = do
+    skip 4
+    ty     <- cToEnum <$> g16
+    flags  <- cToEnum <$> g16
+    seqnum <- g32
+    pid    <- g32
+    return $ Header ty flags seqnum pid
 
-_toEnum :: (Integral a, Enum b) => a -> b
-_toEnum = toEnum . fromIntegral
+getMessage :: MessageType -> Get Message
+getMessage NlmsgError = getMessageError
+getMessage RtmNewlink = getMessageLink
+getMessage RtmDellink = getMessageLink
+getMessage RtmGetlink = getMessageLink
+getMessage a          = error $ "Can't decode message " ++ show a
+
+getMessageError :: Get Message
+getMessageError = do
+    code <- Errno . abs . fromIntegral <$> g32
+    hdr  <- getHeader
+    return $ ErrorMsg code hdr
+
+getMessageLink :: Get Message
+getMessageLink = do
+    skip 2
+    ty    <- cToEnum <$> getWord16be
+    idx   <- g32
+    flags <- g32
+    skip 4
+    return $ LinkMsg ty idx flags
+
+getAttributes :: Get Attributes
+getAttributes = return Attributes
+
+--
+-- Packet serialization
+--
+putPacket :: Packet -> [ByteString]
+putPacket (header, message, attributes) =
+    let attrs  = runPut $ putAttributes attributes
+        msg    = runPut $ putMessage message
+        hdr = runPut $ putHeader (length msg + length attrs + 16) header
+    in [hdr, msg, attrs]
+
+putHeader :: Int -> Header -> Put
+putHeader len (Header ty flags seqnum pid) = do
+    p32 (fromIntegral len)
+    p16 (cFromEnum ty)
+    p16 (cFromEnum flags)
+    p32 seqnum
+    p32 pid
+
+putMessage :: Message -> Put
+putMessage (LinkMsg ty idx flags) = do
+    p8 (cFromEnum AfUnspec) >> p8 0
+    p16 (cFromEnum ty)
+    p32 idx
+    p32 flags
+    p32 0xFFFFFFFF
+putMessage _ = error "Can't transmit this message to the kernel."
+
+putAttributes :: Attributes -> Put
+putAttributes _ = p8 0
+
+--
+-- Helpers
+--
+p8 :: Word8 -> Put
+p8  = putWord8
+p16 :: Word16 -> Put
+p16 = putWord16host
+p32 :: Word32 -> Put
+p32 = putWord32host
+
+g8 :: Get Word8
+g8  = getWord8
+g16:: Get Word16
+g16 = getWord16host
+g32 :: Get Word32
+g32 = getWord32host
